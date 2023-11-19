c1f64682122d1c27a06b47d57ca6da73c59639ef
Daniel Jurek <djurek@microsoft.com>
2023-11-19 03:27:11 +0000

    Parallelism

--- go.ps1
+++ go.ps1
@@ -2,29 +2,56 @@
 param(
     $Triplets = 'x64-linux',
     $OutFile = '',
-    $First = 0
+    $First = 0,
+    $Parallel = 2
 )
+
 function vcpkgDownload($port, $triplet) {
+    
+    $originalLocation = Get-Location
     try {
-        Write-Host "vcpkg/vcpkg install `"$($port):$($triplet)`" --only-downloads ..." 
-        $logs = vcpkg/vcpkg install `
-            "$($port):$($triplet)" `
-            --only-downloads
+        Set-Location ./vcpkg 
+        $setupDuration = Measure-Command { 
+            $worktreeLocation = "../worktrees/$triplet/$($port.Replace('[', '_').Replace(']', '_'))"
+            git worktree add $worktreeLocation 2>&1 | Out-Null
     
-        if ($LASTEXITCODE) {
-            Write-Host "Failed"
-        } else {
-            Write-Host "Succeeded"
+            if ($IsWindows) { 
+                Copy-Item ./vcpkg.exe $worktreeLocation/vcpkg.exe
+            } else { 
+                Copy-Item ./vcpkg $worktreeLocation/vcpkg
+            }
+    
         }
-    
-        return @{ Port = $port; Triplet = $triplet; Logs = $logs; ExitCode = $LASTEXITCODE }
+        Set-Location $worktreeLocation
+
+        $logs = @()
+        $duration = Measure-Command {
+            $logs = ./vcpkg install "$($port):$($triplet)" --only-downloads
+         }
+
+        $logLine = "vcpkg install `"$($port):$($triplet)`" --only-downloads" 
+        if ($LASTEXITCODE) {
+            $logLine += " Failed"
+        } else {
+            $logLine += " Succeeded"
+        }
+        $logLine += " (Vcpkg time: $($duration.TotalSeconds)s, Setup time: $($setupDuration.TotalSeconds)s)"
+        Write-host $logLine
+
+        return @{ 
+            Port = $port; 
+            Triplet = $triplet; 
+            Logs = $logs; 
+            ExitCode = $LASTEXITCODE; 
+            TotalSeconds = $duration.TotalSeconds;
+            SetupSeconds = $setupDuration.TotalSeconds;
+        }
    
     } finally {
-        # Build never completes when using `--only-downloads` so perform 
-        # manual cleaning
-        Push-Location vcpkg
-        git clean -xdf buildtrees/ installed/ packages/ downloads/ | Out-Null
-        Pop-Location
+        Set-Location $originalLocation/vcpkg
+        git worktree remove $worktreeLocation 2>&1 | Write-Host
+
+        Set-Location $originalLocation
     }
 
 }
@@ -48,18 +75,18 @@ function getFeatures($port) {
 # Clone vcpkg
 # TODO: Shallow clone, specify commitish from parameter
 git clone https://github.com/microsoft/vcpkg.git
+
 Push-Location vcpkg
 $commitish = git rev-parse HEAD
-Pop-Location 
 Write-Host "Current vcpkg commitish: $commitish"
 
-
-# Install vcpkg
 if ($IsWindows) {
-    .\vcpkg\bootstrap-vcpkg.bat
+    .\bootstrap-vcpkg.bat
 } else {
-    ./vcpkg/bootstrap-vcpkg.sh
+    ./bootstrap-vcpkg.sh
 }
+Pop-Location 
+
 
 # List vcpkg ports
 $ports = ./vcpkg/vcpkg search --x-json | ConvertFrom-Json -AsHashtable
@@ -69,28 +96,41 @@ Write-Host "Total ports $($ports.Count)"
 # Process ports
 $splitTriplets = $Triplets -split ','
 $results = @()
+$toRun = @()
 $processed = 0
 foreach ($port in $ports.Keys | Sort-Object) {
     $processed++
-    Write-Host "[$(($processed/$ports.Keys.Count).ToString('P'))] Port: $($targetPort.package_name)"
     $targetPort = $ports[$port]
 
     foreach ($triplet in $splitTriplets) {
         $features = getFeatures $targetPort.package_name
 
         if (!$features) { 
-            $results += vcpkgDownload -port $targetPort.package_name -triplet $triplet
+            $toRun += @{ Port = $targetPort.package_name; Triplet = $triplet }
         } else { 
             foreach($feature in $features) { 
-                $results += vcpkgDownload -port "$($targetPort.package_name)[$feature]" -triplet $triplet
+                $toRun += @{ 
+                    Port = "$($targetPort.package_name)[$feature]"; 
+                    Triplet = $triplet 
+                }
             }
         }
     }
 
     if ($First -and $processed -ge $First) { 
-        Write-Host "Processed first $processed ports, exiting"
+        Write-Host "Done queuing, only processing the first $First ports"
         break
     }
+}
+
+Write-Host "Ports to download: $($toRun.Count)"
+
+$stringVcpkgDownload = ${function:vcpkgDownload}.ToString()
+$results = $toRun | ForEach-Object -ThrottleLimit $Parallel -Parallel {
+    # Workaround to enable function call in parallel block. The function could
+    # also be put inline, but it's possible to argue that this is more readable.
+    ${function:vcpkgDownload} = $using:stringVcpkgDownload
+    vcpkgDownload -port $_.Port -triplet $_.Triplet
 }
 
 if ($OutFile) {
@@ -101,7 +141,7 @@ if ($OutFile) {
 
 Write-Host "Summary: "
 Write-host "Ports: $($ports.Keys.Count)"
-Write-Host "Port * Triplets Processed: $($results.Count)"
+Write-Host "Port * Triplets Processed: $($toRun.Count)"
 Write-Host "Succeeded: $($results.Where({$_.ExitCode -eq 0}).Count)"
 Write-Host "Failed: $($results.Where({$_.ExitCode -ne 0}).Count)"
 
